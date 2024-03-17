4388bc32dd855f3f8dac5dd3ef7b0a6213029a98
surajn581 <surajn581@gmail.com>
2024-03-17 20:49:46 +0530

    added a ping to redis in conn.py

--- conn.py
+++ conn.py
@@ -5,3 +5,6 @@ file that has the redis client connection object
 import redis
 from queue_config import HOST, PORT, DB
 RedisConn = redis.Redis(host=HOST, port=PORT, db=DB)
+
+#ping the redis server, fail if fails
+RedisConn.ping()
