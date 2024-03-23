d8bf96af621e887adeec61676cd34349c350831c
surajn581 <surajn581@gmail.com>
2024-03-24 01:30:52 +0530

    heartbeat of the dead worker is deleted

--- worker_monitor.py
+++ worker_monitor.py
@@ -5,11 +5,13 @@ from datetime import datetime
 from conn import RedisConn as conn
 from utils import logger
 
+LIVENESS_THRESHOLD = 120
 
 class WorkerMonitor:
-    def __init__(self):
+    def __init__(self, liveness_threshold = LIVENESS_THRESHOLD):
         self.name = os.environ.get('HOSTNAME', self.__class__.__name__ + uuid.uuid4().hex)
         self.conn = conn
+        self.liveness_threshold = liveness_threshold
 
     def monitor(self):
         while True:
@@ -20,17 +22,22 @@ class WorkerMonitor:
 
     def declare_dead_worker(self, worker):
         self.conn.sadd('dead_workers', worker)
+
+    def _handle_dead_worker(self, worker, timestamp):
+        logger.info(f'declaring worker: {worker} dead, last timestamp: {timestamp}')
+        self.declare_dead_worker(worker)
+        logger.info(f'removing worker: {worker} hearbeat')
+        self.conn.hdel('heartbeats', worker)
     
     def _monitor(self):
         heartbeats = self.conn.hgetall('heartbeats')
         for worker, timestamp in heartbeats.items():
             if self.conn.sismember('dead_workers', worker):
                 continue
-            if not datetime.now().timestamp() - float(timestamp) > 120:
+            if not datetime.now().timestamp() - float(timestamp) > self.liveness_threshold:
                 logger.info(f'worker: {worker} is live')
                 continue
-            logger.info(f'declaring worker: {worker} dead')
-            self.declare_dead_worker(worker)
+            self._handle_dead_worker(worker, timestamp)
 
 def main():
     monitor = WorkerMonitor()
