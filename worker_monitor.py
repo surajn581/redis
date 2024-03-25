e27ad1a59cf06227ef80cf1ed3d570e8059a6cc9
surajn581 <surajn581@gmail.com>
2024-03-25 14:34:44 +0530

    added logic that checks if the worker is stuck waiting for work before declaring it dead. if the worker is stuck waiting for work, it's considered alive for 5 checks before being considerd as dead

--- worker_monitor.py
+++ worker_monitor.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from conn import RedisConn as conn
 from utils import logger
 
-LIVENESS_THRESHOLD = 120
+LIVENESS_THRESHOLD = 60
 
 class WorkerMonitor:
     def __init__(self, liveness_threshold = LIVENESS_THRESHOLD):
@@ -17,27 +17,37 @@ class WorkerMonitor:
         while True:
             if not self.conn.hlen('heartbeats'):
                 continue
-            self._monitor()            
-            time.sleep(120)
+            self._monitor()
+            time.sleep(int(self.liveness_threshold/2))
 
     def declare_dead_worker(self, worker):
         self.conn.sadd('dead_workers', worker)
 
-    def _handle_dead_worker(self, worker, timestamp):
-        logger.info(f'declaring worker: {worker} dead, last timestamp: {timestamp}')
+    def _handle_dead_worker(self, worker):
         self.declare_dead_worker(worker)
         logger.info(f'removing worker: {worker} hearbeat')
         self.conn.hdel('heartbeats', worker)
+
+    def is_worker_live(self, worker, timestamp):
+        delta = datetime.now().timestamp() - float(timestamp)
+        if delta < self.liveness_threshold:
+            return True
+        logger.info(f'worker: {worker} has timestamp older than {self.liveness_threshold} seconds')
+        if not self.conn.scard(worker):
+            logger.info(f'worker: {worker} does not have any pending work items, it may not be dead')
+            return True if delta <= self.liveness_threshold*10 else False
+        logger.info(f'worker: {worker} is dead with last timestamp: {timestamp}')
+        return False
     
     def _monitor(self):
         heartbeats = self.conn.hgetall('heartbeats')
         for worker, timestamp in heartbeats.items():
             if self.conn.sismember('dead_workers', worker):
                 continue
-            if not datetime.now().timestamp() - float(timestamp) > self.liveness_threshold:
+            if self.is_worker_live(worker, timestamp):
                 logger.info(f'worker: {worker} is live')
-                continue
-            self._handle_dead_worker(worker, timestamp)
+                continue            
+            self._handle_dead_worker(worker)
 
 def main():
     monitor = WorkerMonitor()
